1f54bcbddb317b9435932fcac83fd8a3eb0410e6
Vasiliy VV <vase3k@gmail.com>
2024-03-29 10:09:00 +0300

    40 methods

--- dist/script.js
+++ dist/script.js
@@ -117,6 +117,41 @@ _core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.find = function (selecto
   }
   return this;
 };
+_core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.closest = function (selector) {
+  let counter = 0;
+  for (let i = 0; i < this.length; i++) {
+    this[i] = this[i].closest(selector) === null ? 'classNotFound' : this[i].closest(selector);
+    counter++;
+  }
+  const objLength = Object.keys(this).length;
+  for (; counter < objLength; counter++) {
+    delete this[counter];
+  }
+  console.log(this);
+  return this;
+};
+_core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.siblings = function () {
+  let numberOfItems = 0;
+  let counter = 0;
+  const copyObj = Object.assign({}, this);
+  for (let i = 0; i < copyObj.length; i++) {
+    const arr = copyObj[i].parentNode.children;
+    for (let j = 0; j < arr.length; j++) {
+      if (copyObj[i] === arr[j]) {
+        continue;
+      }
+      this[counter] = arr[j];
+      counter++;
+    }
+    numberOfItems += arr.length - 1;
+  }
+  this.length = numberOfItems;
+  const objLength = Object.keys(this).length;
+  for (; numberOfItems < objLength; numberOfItems++) {
+    delete this[numberOfItems];
+  }
+  return this;
+};
 
 /***/ }),
 
@@ -298,13 +333,6 @@ var __webpack_exports__ = {};
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _lib_lib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./lib/lib */ "./src/js/lib/lib.js");
 
-$('button').on('click', function () {
-  $('div').eq(1).toggleClass('active');
-});
-$('div').click(function () {
-  console.log($(this).index());
-});
-console.log($('div').eq(2).find('.more'));
 })();
 
 /******/ })()
--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,1 @@
 import './lib/lib';
-
-$('button').on('click', function (){
-    $('div').eq(1).toggleClass('active');
-});
-
-$('div').click(function() {
-    console.log($(this).index());
-});
-
-console.log($('div').eq(2).find('.more'));
