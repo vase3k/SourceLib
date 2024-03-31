666c42d38b631e29a99831789fa3f3c9c3adf853
Vasiliy VV <vase3k@gmail.com>
2024-03-31 18:32:19 +0300

    46 dynamc drown

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,46 @@
-import './lib/lib';
+import $ from './lib/lib';
 
-$('.wrap').html(
-    `<div class="dropdown">
-        <button class="btn btn-primary dropdown-toggle" id="dropdownMenuButton">
-            dropdown button
-        </button>
-        <div class="dropdown-menu" data-toggle-id="dropdownMenuButton">
-            <div href="#" class="dropdown-item">action #1</div>
-            <div href="#" class="dropdown-item">action #2</div>
-            <div href="#" class="dropdown-item">action #3</div>
-        </div>
-    </div>`
-);
+$('#first').on('click', () => {
+    $('div').eq(1).fadeToggle(800);
+});
 
-$('.dropdown-toggle').dropdown();
+$('[data-count="second"]').on('click', () => {
+    $('div').eq(2).fadeToggle(800);
+});
+
+$('button').eq(2).on('click', () => {
+    $('.w-500').fadeToggle(800);
+});
+
+$('#trigger').click(() => $('#trigger').createModal({
+    text: {
+        title: 'Modal title',
+        body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum minus doloremque nesciunt enim rem quam corporis? Dolorem pariatur magnam distinctio perferendis. Ratione dolorem voluptates iusto facilis odit veritatis, suscipit voluptatibus!'
+    },
+    btns: {
+        count: 3,
+        settings: [
+            [
+                'Close',
+                ['btn-danger', 'mr-10'],
+                true
+            ],
+            [
+                'Save changes',
+                ['btn-success'],
+                false,
+                () => {
+                    alert('Данные сохранены');
+                }
+            ],
+            [
+                'Another btn',
+                ['btn-warning', 'ml-10'],
+                false,
+                () => {
+                    alert('Hello World');
+                }
+            ]
+        ]
+    }
+}));
